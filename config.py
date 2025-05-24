09eb0889ace381b996d016ed6028c98d0f7eaa63
OkyereI <isaacokyere4@gmail.com>
2025-05-24 22:16:34 +0000

    adde password

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'instance', 'community.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     # Arkesel SMS API Configuration
     ARKESEL_API_KEY = os.environ.get('ARKESEL_API_KEY') or 'b0FrYkNNVlZGSmdrendVT3hwUHk'
     ARKESEL_SENDER_ID = 'KenyasiN1YA' # Or your registered sender ID
